9c0ab0a3c558fb7996e3722ed70b9e63c3a5a126
Anna Kalynchuk <anya.kalinchuk.94@gmail.com>
2024-03-25 13:43:48 +0100

    changed file vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://cleanuri.com/api/v1/shorten',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
-})
+});
