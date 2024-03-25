8c257a6f1af15d1f4e2abc60e0e435594e4c4043
Anna Kalynchuk <anya.kalinchuk.94@gmail.com>
2024-03-25 13:50:54 +0100

    Added base url

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/more-than-just-shorter-links-for-deploy/",
   plugins: [react()],
 });
